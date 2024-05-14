d31d855a269355525fb2f7579dbf86dbfb41db7a
renaudbaussart <renaud.baussart@proton.me>
2024-05-14 17:50:24 +0200

    feat(variable-type.js): create a file to show all type of variable in js

---
+++ variable-type/variable-type.js
@@ -0,0 +1,21 @@
+//number type is for all type of number whole number, decimal number,negative ....
+let typeNumber = 3;
+
+//string type is for all string of char and simple char
+let typeString = 'hello world';
+
+//boolean type is true or false variable 
+let typeBoolean = true;
+
+//undefined type is specific type it is literaly not defined
+let typeUndefined = undefined;
+
+//null type is in lot of situation equivalent to 0 / false / ""
+let typeNull = null;
+
+//bigint is for number that have more value than (2**53-1)
+let typeBigint = 1234567890123456789012345678901234567890n;
+
+// objet and symbol is a special type of var
+let typeObjet = new Object();
+let typeIcon = Symbol();
