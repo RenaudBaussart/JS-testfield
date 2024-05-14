24497afd7f55936070624e15ec75d4992c10cbf1
renaudbaussart <renaud.baussart@proton.me>
2024-05-14 18:22:51 +0200

    feat(list-of-methode.js): create file to list methode and add front one

---
+++ methode/list-of-methode.js
@@ -0,0 +1,15 @@
+/*
+methode are specific interaction that are link with front end like google,firefox... or back end with nodejs...
+let start with -----------------front end methode----------------------
+*/
+
+//alert are methode that will show a pop up with a message inside
+alert("Hello");
+
+//prompt are methode that will show a pop up with a message inside and will wait for a answer in the prompt part
+let resultPrompt = prompt(title, [""]);
+
+//confirm are methode that will show a pop up with a message inside and ask you if the message is correct and will return a true or a false
+let resultConfirm = confirm("do you wish to confirme");
+
+//------------------------------back end methode-----------------------------
