960d53036fb00536963df87f9518f9a8a4b93f4d
renaudbaussart <renaud.baussart@proton.me>
2024-05-14 18:45:41 +0200

    feat(type-convertion.js): create a file for type conversion

---
+++ variable/type-convertion.js
@@ -0,0 +1,16 @@
+//---------------------string convertion convert variable into string---------------------
+
+// you can convert boolean true or false will be convert as it is into a string
+let booleanConvetibleValue = true;
+alert(typeof booleanConvetibleValue);
+let stringConvertedValue = String(booleanConvetibleBool);
+alert(typeof stringConvertedBool);//will show string
+alert(stringConvertedValue);//will show "true"
+
+// you can convert number into string 
+let numberConvetibleValue = 345;
+alert(typeof numberConvetibleValue);
+stringConvertedValue = string(numberConvetibleValue);
+alert(typeof stringConvertedValue);//will show string
+alert(stringConvertedValue);//will show "345"
+
