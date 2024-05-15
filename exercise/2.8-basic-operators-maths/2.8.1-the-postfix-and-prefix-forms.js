6aee4ebd36153acad46fe1626098a870ce5f40d1
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:19:29 +0200

    feat(2.8.1-the-postfix-and-prefix-forms): done exercise

---
+++ exercise/2.8-basic-operators-maths/2.8.1-the-postfix-and-prefix-forms.js
@@ -0,0 +1,16 @@
+/*
+What are the final values of all variables a, b, c and d after the code below?
+-----------------
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+-----------------
+the value of the variable a will be 2
+the value of the variable b will be 2
+the value of the variable c will be 2
+the value of the variable d will be 1
+beacause the ++a is a prefix incrementation and b++ is a postfix incrementation
+*/
+
+
