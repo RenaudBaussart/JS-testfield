ae6934b89394cfc79f88b25869ed10ed98eae74a
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:20:40 +0200

    feat(2.8.3-type-convesions): done exercise

---
+++ exercise/2.8-basic-operators-maths/2.8.3-type-convesions.js
@@ -0,0 +1,31 @@
+/*
+What are results of these expressions?
+
+"" + 1 + 0 | 10 | string + ? = concatenate
+
+"" - 1 + 0 | -1 | string - ? = convert + math operation
+
+true + false | 1 | true mean 1 and false 0 so 1 + 0 = 1 = true 
+
+6 / "3" | 2 | string / ? = convert + math operation
+
+"2" * "3" | 6 | string * ? = convert + math operation
+
+4 + 5 + "px" | "9px" | string + ? = concatenate
+
+"$" + 4 + 5 | "$45" | string + ? = concatenate
+
+"4" - 2 | 2 | string - ? = convert + math operation
+
+"4px" - 2 | NaN | string + ? = convert but 4px is NaN 
+
+"  -9  " + 5 | "  -9  5" | string + ? = concatenate
+
+"  -9  " - 5 | -14 | string - ? = convert + math operation
+
+null + 1 | 1 null | numericali converted = 0 sot 0 + 1
+
+undefined + 1 | NaN | undefined is always NaN when numericaly convert
+
+" \t \n" - 2 | -2 | \t \n is non readead caracter so the string is basicli "" so 0 - 2 = -2  
+*/
