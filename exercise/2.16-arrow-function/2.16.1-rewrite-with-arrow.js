ca3970e936627ba8619bacd8156b04fa1ffdece0
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 16:20:11 +0200

    style(2.16.1-rewrite-with-arrow.js): move spacing places

--- exercise/2.16-arrow-function/2.16.1-rewrite-with-arrow.js
+++ exercise/2.16-arrow-function/2.16.1-rewrite-with-arrow.js
@@ -12,13 +12,12 @@ ask(
   function() { alert("You canceled the execution."); }
 );
 */
+
 function ask(question, yes, no) {
     if (confirm(question)) yes();
     else no();
   }
-  
-  ask(
-    "Do you agree?",
+  ask("Do you agree?",
     () => alert("You agreed."),
     () => alert("You canceled the execution.")
   );
