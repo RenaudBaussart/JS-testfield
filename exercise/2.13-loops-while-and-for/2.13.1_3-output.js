45eca7f8c71da8791d7072dc3db5f8a09927c037
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 15:30:55 +0200

    feat(2.13.1_3-output): exercise done

--- exercise/2.13-loops-while-and-for/2.13.1_3-output.js
+++ exercise/2.13-loops-while-and-for/2.13.1_3-output.js
@@ -8,4 +8,40 @@ while (i) {
 }
 ---------------
 1 will be print last
+
+------------------------------------------------------------------------------------
+
+For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+Both loops alert the same values, or not?
+
+The prefix form ++i:
+----------------------------
+let i = 0;
+while (++i < 5) alert( i );
+----------------------------
+The postfix form i++
+----------------------------
+let i = 0;
+while (i++ < 5) alert( i );
+----------------------------
+the prefix form will alert 1  2  3  4
+the postfix form will alert  1  2  3  4  5  
+
+-------------------------------------------------------------------------------------
+
+For each loop write down which values it is going to show. Then compare with the answer.
+
+Both loops alert same values or not?
+
+The postfix form:
+---------------------------------------
+for (let i = 0; i < 5; i++) alert( i );
+---------------------------------------
+The prefix form:
+---------------------------------------
+for (let i = 0; i < 5; ++i) alert( i );
+---------------------------------------
+the prefix form will alert 0  1  2  3  4
+the postfix form will alert 0  1  2  3  4  
 */
