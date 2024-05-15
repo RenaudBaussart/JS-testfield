1a12b1865c130f8793efeedb9b87da0f6e6cb0df
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:19:53 +0200

    feat(2.8.2-assignment-result): done exercise

---
+++ exercise/2.8-basic-operators-maths/2.8.2-assignment-result.js
@@ -0,0 +1,10 @@
+/*
+What are the values of a and x after the code below?
+---------------------
+let a = 2;
+
+let x = 1 + (a *= 2);
+---------------------
+the value of the variable a will be 2
+the value of the variable x will be 5
+*/
