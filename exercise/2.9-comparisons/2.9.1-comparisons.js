0b0c307e6b73a483e1ebff1d5137a684945b8ec8
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:21:23 +0200

    feat(2.9.1-comparisons): done exercise

---
+++ exercise/2.9-comparisons/2.9.1-comparisons.js
@@ -0,0 +1,12 @@
+/*
+What will be the result for these expressions?
+
+5 > 4 | true | 5 si greater than 4 
+"apple" > "pineapple" | false | the fist later for the two word is a and p in the ascii table p is greater than a 
+"2" > "12" | true | 2 in the ascii table is greater than 1
+undefined == null | true | undefined is always == to null and the other way around 
+undefined === null | false | === mean == and same type so false
+null == "\n0\n" | false |  null only equals undefined
+null === +"\n0\n" | false | null only equals undefined
+ 
+*/
