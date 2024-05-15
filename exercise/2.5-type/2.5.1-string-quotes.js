5989e459bb5471924cad4e3b44e8ac75d8986aca
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:18:45 +0200

    feat(2.5.1-string-quotes): done exercise

---
+++ exercise/2.5-type/2.5.1-string-quotes.js
@@ -0,0 +1,16 @@
+/*
+What is the output of the script?
+------------------------------------------
+let name = "Ilya";
+
+alert( `hello ${1}` );
+
+alert( `hello ${"name"}` );
+
+alert( `hello ${name}` ); 
+------------------------------------------
+will do 3 popup
+1- hello 1
+2- hello name
+3- hello Ilya
+*/
