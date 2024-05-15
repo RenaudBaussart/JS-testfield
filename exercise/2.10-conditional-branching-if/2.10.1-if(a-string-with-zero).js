1d12e373238307dd5406702cfb4c0cd7b367a94a
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:21:51 +0200

    feat(2.10.1-if(a-string-with-zero)): done exercise

---
+++ exercise/2.10-conditional-branching-if/2.10.1-if(a-string-with-zero).js
@@ -0,0 +1,9 @@
+/*
+Will alert be shown?
+-------------------
+if ("0") {
+  alert( 'Hello' );
+}
+-------------------
+the if() convert thing inside scoop in boolean so "0" convert in bool is true cause there is someting in the string
+*/
