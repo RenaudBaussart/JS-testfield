8a0c9fe9229657f856b661b85f3aa73db64aaf3c
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:24:07 +0200

    feat(2.11.7-check-the-range-outside): done exercise

---
+++ exercise/2.11-logical-operators/2.11.7-check-the-range-outside.js
@@ -0,0 +1,6 @@
+/*
+Write an if condition to check that age is NOT between 14 and 90 inclusively.
+
+Create two variants: the first one using NOT !, the second one – without it.
+*/
+if(!getUserAge < 91 && getUserAge > 13){}
