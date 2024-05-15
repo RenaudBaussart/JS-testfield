60eb76185c00f20c9a19eb49a4897a35e160da19
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-05-15 13:23:50 +0200

    feat(2.11.6-check-the-range-between): done exercise

---
+++ exercise/2.11-logical-operators/2.11.6-check-the-range-between.js
@@ -0,0 +1,6 @@
+/*
+Write an if condition to check that age is between 14 and 90 inclusively.
+
+“Inclusively” means that age can reach the edges 14 or 90.
+*/
+if(getUserAge < 91 && getUserAge > 13){}
