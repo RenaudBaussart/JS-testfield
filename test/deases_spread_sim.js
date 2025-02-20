479f2b1cfddfa0d8eeefb0c0fac7a680132e534f
Renaud Baussart <renaud.baussart@proton.me>
2025-02-20 09:52:05 +0100

    feat(deases_spread_sim): add error message to the Person Class
    create the class Person with the field infected

--- test/deases_spread_sim.js
+++ test/deases_spread_sim.js
@@ -1,10 +1,15 @@
 class Person{
     constructor(isInfected){
+        if(typeof isInfected !== "boolean"){
+            print("ERROR: the type is not a boolean so returned null")
+            return null
+        }
         this.infected = isInfected;
     }
 
-    static DoesHeSpreads( personeTested){
+    static DoesHeSpreads(personeTested){
         if(personeTested.infected == false){
+            print("ERROR: this person was not infected")
             return false
         }
         else{
@@ -20,3 +25,4 @@ class Person{
         this.infected = true
     }
 }
+
